defce0b5f38da9645699eead26365a67afe97060
Jacob <learningmachine999@gmail.com>
2024-09-18 15:47:04 -0700

    TOC update

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 export default function TOC() {
-    return (
+  return (
+    <div>
       <ul>
         <li><Link to="/Labs">Labs</Link></li>
         <li><Link to="/Labs/Lab1">Lab 1</Link></li>
@@ -8,6 +9,9 @@ export default function TOC() {
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
         <li><Link to="/Kanbas">Kanbas</Link></li>
       </ul>
-    );
-  }
-  
+
+      <a href="https://github.com/weijianhuang21/kanbas-react-web-app/tree/a1"
+        id="wd-github" target="_blank" rel="noopener noreferrer">Github</a>
+    </div>
+  );
+}
