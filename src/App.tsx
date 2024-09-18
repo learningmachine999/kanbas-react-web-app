6f6b993ffbbff3aaee790acf9baa74c51e1c1188
Jacob <learningmachine999@gmail.com>
2024-09-18 15:26:32 -0700

    a1 for submission

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import './App.css';
-import Labs from './Labs';
+import Labs from "./Labs";
+import Kanbas from "./Kanbas";
 
-function App() {
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
+
+export default function App() {
   return (
-    <div>
-      <h1>Welcome to Web Dev</h1>
-    <Labs />
-    </div>
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="Kanbas" />} />
+          <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kanbas/*" element={<Kanbas />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
-export default App;
